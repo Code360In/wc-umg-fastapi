79e0e3c77e04097c0e4bebc860f296eebfd5e373
Franzua Andrez <edson.andrez@outlook.es>
2021-09-05 21:19:23 -0600

    get one resource  cruds

--- crud.py
+++ crud.py
@@ -10,13 +10,29 @@ def get_doctores(db: Session, skip: int = 0, limit: int = 100):
     return db.query(doctor.DoctorList).offset(skip).limit(limit).all()
 
 
+def get_doctor(db: Session, doctor_id: int):
+    return db.query(doctor.DoctorList).filter(doctor.DoctorList.id == doctor_id).first()
+
+
 def get_pacientes(db: Session, skip: int = 0, limit: int = 100):
     return db.query(paciente.PacienteList).offset(skip).limit(limit).all()
+
+
+def get_paciente(db: Session, paciente_id: int):
+    return db.query(paciente.PacienteList).filter(paciente.PacienteList.id == paciente_id).first()
 
 
 def get_enfermedades(db: Session, skip: int = 0, limit: int = 100):
     return db.query(enfermedad.EnfermedadList).offset(skip).limit(limit).all()
 
 
+def get_enfermedad(db: Session, enfermedad_id: int):
+    return db.query(enfermedad.EnfermedadList).filter(enfermedad.EnfermedadList.id == enfermedad_id).first()
+
+
 def get_medicamentos(db: Session, skip: int = 0, limit: int = 100):
     return db.query(medicamento.MedicamentoList).offset(skip).limit(limit).all()
+
+
+def get_medicamento(db: Session, medicamento_id: int):
+    return db.query(medicamento.MedicamentoList).filter(medicamento.MedicamentoList.id == medicamento_id).first()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
@@ -28,10 +28,26 @@ def read_doctores(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
     return doctores
 
 
+@app.get("/doctores/{doctor_id}", response_model=schemas.Doctor)
+def read_doctor(doctor_id: int, db: Session = Depends(get_db)):
+    doctor = crud.get_doctor(db, doctor_id=doctor_id)
+    if doctor is None:
+        raise HTTPException(status_code=404, detail="Doctor No encontrado")
+    return doctor
+
+
 @app.get("/pacientes/", response_model=List[schemas.Paciente])
 def read_pacientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     pacientes = crud.get_pacientes(db, skip=skip, limit=limit)
     return pacientes
+
+
+@app.get("/pacientes/{paciente_id}", response_model=schemas.Paciente)
+def read_paciente(paciente_id: int, db: Session = Depends(get_db)):
+    paciente = crud.get_paciente(db, paciente_id=paciente_id)
+    if paciente is None:
+        raise HTTPException(status_code=404, detail="Paciente No encontrado")
+    return paciente
 
 
 @app.get("/enfermedades/", response_model=List[schemas.Enfermedad])
@@ -40,7 +56,23 @@ def read_enfermedades(skip: int = 0, limit: int = 100, db: Session = Depends(get
     return enfermedades
 
 
+@app.get("/enfermedades/{emfermedad_id}", response_model=schemas.Enfermedad)
+def read_enfermedad(emfermedad_id: int, db: Session = Depends(get_db)):
+    enfermedad = crud.get_enfermedad(db, emfermedad_id=emfermedad_id)
+    if enfermedad is None:
+        raise HTTPException(status_code=404, detail="Enfermedad No encontrada")
+    return enfermedad
+
+
 @app.get("/medicamentos/", response_model=List[schemas.Medicamento])
 def read_medicamentos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     medicamentos = crud.get_medicamentos(db, skip=skip, limit=limit)
     return medicamentos
+
+
+@app.get("/medicamentos/{medicamento_id}", response_model=schemas.Medicamento)
+def read_medicamento(medicamento_id: int, db: Session = Depends(get_db)):
+    medicamento = crud.get_medicamento(db, medicamento_id=medicamento_id)
+    if medicamento is None:
+        raise HTTPException(status_code=404, detail="Medicamento No encontrado")
+    return medicamento
